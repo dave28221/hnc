abf2d65bbeda104f3051f937575e597c40d08f2a
Dave B <davebova@hotmail.com>
2025-09-24 15:04:22 +0800

    push

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -11,13 +11,17 @@ type application struct {
 	templateCache map[string]*template.Template
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello")
 }
 
 func main() {
+	app := &application{
+		templateCache: make(map[string]*template.Template),
+	}
+
 	router := http.NewServeMux()
-	router.HandleFunc("GET /", handler)
+	router.HandleFunc("GET /", app.home)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 
