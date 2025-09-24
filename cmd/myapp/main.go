186f848c6332c8c035fc882613e6b59cab11fd45
David Bova <davidbova@Davids-MacBook-Air.local>
2025-09-24 21:25:41 +0800

    update router

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -7,8 +7,13 @@ import (
 
 func main() {
 
-	http.Handle("/", http.FileServer(http.Dir("./ui/html")))
-	err := http.ListenAndServe(":8080", nil)
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./ui/html/index.html")
+	})
+
+	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal(err)
 	}
