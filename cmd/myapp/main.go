4731c9695b33b7e3afeb011468b3e957a200c590
David Bova <davidbova@Davids-MacBook-Air.local>
2025-09-25 20:34:33 +0800

    glob

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func main() {
+	//parse static files including css and js//
 
 	router := http.NewServeMux()
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./ui/html/index.html")
+		tmpl := template.New("")
+		tmpl.ParseGlob("html/*.html")
+		for _, t := range tmpl.Templates() {
+			fmt.Println(t.Name())
+		}
+
 	})
 
+	fmt.Println("server is up and running on 8080")
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal(err)
